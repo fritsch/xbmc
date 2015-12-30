9c5bf064c68186ed60d5bf00a66c0bf89d949592
Christian Fetzer <fetzer.ch@googlemail.com>
2015-12-30 18:49:49 +0100

    [cmake] More work on Windows support

---
+++ project/cmake/modules/FindD3DX11Effects.cmake
@@ -0,0 +1,21 @@
+# - Try to find D3DX11Effects
+# Once done this will define
+#
+# D3DX11EFFECTS_FOUND - system has D3DX11Effects
+# D3DX11EFFECTS_INCLUDE_DIRS - the D3DX11Effects include directories
+# D3DX11EFFECTS_LIBRARIES - the D3DX11Effects libraries
+
+include(ExternalProject)
+ExternalProject_Add(d3dx11effects
+            SOURCE_DIR ${CORE_SOURCE_DIR}/lib/win32/Effects11
+            PREFIX ${CORE_BUILD_DIR}/Effects11
+            CONFIGURE_COMMAND ""
+            BUILD_COMMAND devenv /build ${CMAKE_BUILD_TYPE}
+                          ${CORE_SOURCE_DIR}/lib/win32/Effects11/Effects11_2013.sln
+            INSTALL_COMMAND "")
+
+set(D3DX11EFFECTS_FOUND 1)
+set(D3DX11EFFECTS_INCLUDE_DIRS ${CORE_SOURCE_DIR}/lib/win32/Effects11/inc)
+mark_as_advanced(D3DX11EFFECTS_FOUND)
+
+# TODO: core_link_library
