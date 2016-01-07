aa79ed4e3562cfb9a3f51177354831d721a8963b
Christian Fetzer <fetzer.ch@googlemail.com>
2016-01-07 17:57:12 +0100

    [cmake] Disable non relevant parts of DllLoader for Windows
    This is not fully functional at the moment and needs more work after
    compilation and linking is working on Windows.

--- project/cmake/scripts/windows/macros.cmake
+++ project/cmake/scripts/windows/macros.cmake
@@ -1,41 +1,41 @@
 function(core_link_library lib wraplib)
-  set(export -Wl,--unresolved-symbols=ignore-all
-             `cat ${CMAKE_BINARY_DIR}/${CORE_BUILD_DIR}/cores/dll-loader/exports/wrapper.def`
-             ${CMAKE_BINARY_DIR}/${CORE_BUILD_DIR}/cores/dll-loader/exports/CMakeFiles/wrapper.dir/wrapper.c.o)
-  set(check_arg "")
-  if(TARGET ${lib})
-    set(target ${lib})
-    set(link_lib ${CMAKE_BINARY_DIR}/${CORE_BUILD_DIR}/${lib}/${lib}.a)
-    set(check_arg ${ARGV2})
-    set(data_arg  ${ARGV3})
-  else()
-    set(target ${ARGV2})
-    set(link_lib ${lib})
-    set(check_arg ${ARGV3})
-    set(data_arg ${ARGV4})
-  endif()
-  if(check_arg STREQUAL "export")
-    set(export ${export} 
-        -Wl,--version-script=${ARGV3})
-  elseif(check_arg STREQUAL "nowrap")
-    set(export ${data_arg})
-  elseif(check_arg STREQUAL "extras")
-    foreach(arg ${data_arg})
-      list(APPEND export ${arg})
-    endforeach()
-  endif()
-  get_filename_component(dir ${wraplib} PATH)
-  add_custom_command(OUTPUT ${wraplib}-${ARCH}${CMAKE_SHARED_MODULE_SUFFIX}
-                     COMMAND cmake -E make_directory ${dir}
-                     COMMAND ${CMAKE_C_COMPILER}
-                     ARGS    -Wl,--whole-archive
-                             ${link_lib}
-                             -Wl,--no-whole-archive -lm
-                             -shared -o ${CMAKE_BINARY_DIR}/${wraplib}-${ARCH}${CMAKE_SHARED_MODULE_SUFFIX}
-                             ${export}
-                     DEPENDS ${target} wrapper.def wrapper)
-  list(APPEND WRAP_FILES ${wraplib}-${ARCH}${CMAKE_SHARED_MODULE_SUFFIX})
-  set(WRAP_FILES ${WRAP_FILES} PARENT_SCOPE)
+#  set(export -Wl,--unresolved-symbols=ignore-all
+#             `cat ${CMAKE_BINARY_DIR}/${CORE_BUILD_DIR}/cores/dll-loader/exports/wrapper.def`
+#             ${CMAKE_BINARY_DIR}/${CORE_BUILD_DIR}/cores/dll-loader/exports/CMakeFiles/wrapper.dir/wrapper.c.o)
+#  set(check_arg "")
+#  if(TARGET ${lib})
+#    set(target ${lib})
+#    set(link_lib ${CMAKE_BINARY_DIR}/${CORE_BUILD_DIR}/${lib}/${lib}.a)
+#    set(check_arg ${ARGV2})
+#    set(data_arg  ${ARGV3})
+#  else()
+#    set(target ${ARGV2})
+#    set(link_lib ${lib})
+#    set(check_arg ${ARGV3})
+#    set(data_arg ${ARGV4})
+#  endif()
+#  if(check_arg STREQUAL "export")
+#    set(export ${export}
+#        -Wl,--version-script=${ARGV3})
+#  elseif(check_arg STREQUAL "nowrap")
+#    set(export ${data_arg})
+#  elseif(check_arg STREQUAL "extras")
+#    foreach(arg ${data_arg})
+#      list(APPEND export ${arg})
+#    endforeach()
+#  endif()
+#  get_filename_component(dir ${wraplib} PATH)
+#  add_custom_command(OUTPUT ${wraplib}-${ARCH}${CMAKE_SHARED_MODULE_SUFFIX}
+#                     COMMAND cmake -E make_directory ${dir}
+#                     COMMAND ${CMAKE_C_COMPILER}
+#                     ARGS    -Wl,--whole-archive
+#                             ${link_lib}
+#                             -Wl,--no-whole-archive -lm
+#                             -shared -o ${CMAKE_BINARY_DIR}/${wraplib}-${ARCH}${CMAKE_SHARED_MODULE_SUFFIX}
+#                             ${export}
+#                     DEPENDS ${target} wrapper.def wrapper)
+#  list(APPEND WRAP_FILES ${wraplib}-${ARCH}${CMAKE_SHARED_MODULE_SUFFIX})
+#  set(WRAP_FILES ${WRAP_FILES} PARENT_SCOPE)
 endfunction()
 
 function(find_soname lib)
