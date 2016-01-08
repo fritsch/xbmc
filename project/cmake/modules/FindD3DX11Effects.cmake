573d9743c7bc22d3870ee70c372a4ec2aaa9ff50
Christian Fetzer <fetzer.ch@googlemail.com>
2016-01-08 15:39:36 +0100

    [cmake] Install needed DLLs for Windows

--- project/cmake/modules/FindD3DX11Effects.cmake
+++ project/cmake/modules/FindD3DX11Effects.cmake
@@ -3,8 +3,7 @@
 #
 # D3DX11EFFECTS_FOUND - system has D3DX11Effects
 # D3DX11EFFECTS_INCLUDE_DIRS - the D3DX11Effects include directories
-#
-# and link Kodi against the D3DX11Effects libraries.
+# D3DCOMPILER_DLL - Path to the Direct3D Compiler
 
 include(ExternalProject)
 ExternalProject_Add(d3dx11effects
@@ -19,3 +18,15 @@ set(D3DX11EFFECTS_FOUND 1)
 set(D3DX11EFFECTS_INCLUDE_DIRS ${CORE_SOURCE_DIR}/lib/win32/Effects11/inc)
 set(D3DX11EFFECTS_LIBRARIES ${CORE_SOURCE_DIR}/lib/win32/Effects11/libs/Effects11/${CMAKE_BUILD_TYPE}/Effects11.lib)
 mark_as_advanced(D3DX11EFFECTS_FOUND)
+
+find_file(D3DCOMPILER_DLL
+          NAMES d3dcompiler_47.dll d3dcompiler_46.dll
+          PATHS
+            "[HKEY_LOCAL_MACHINE\\SOFTWARE\\Wow6432Node\\Microsoft\\Microsoft SDKs\\Windows\\v8.1;InstallationFolder]/Redist/D3D/x86"
+            "[HKEY_LOCAL_MACHINE\\SOFTWARE\\Wow6432Node\\Microsoft\\Microsoft SDKs\\Windows\\v8.0;InstallationFolder]/Redist/D3D/x86"
+            "$ENV{WindowsSdkDir}redist/d3d/x86"
+          NO_DEFAULT_PATH)
+if(NOT D3DCOMPILER_DLL)
+  message(WARNING "Could NOT find Direct3D Compiler")
+endif()
+mark_as_advanced(D3DCOMPILER_DLL)
--- project/cmake/scripts/windows/macros.cmake
+++ project/cmake/scripts/windows/macros.cmake
@@ -152,3 +152,21 @@ function(add_shader_dx target hlsl profile entrypoint)
   target_sources(${target} PRIVATE ${file}.h)
   target_include_directories(${target} PRIVATE ${CMAKE_CURRENT_BINARY_DIR})
 endfunction()
+
+# Copies the main dlls to the root of the buildtree
+# On return:
+#   files added to ${install_data}, mirror in build tree
+function(copy_main_dlls_to_buildtree)
+  set(dir ${PROJECT_SOURCE_DIR}/../Win32BuildSetup/dependencies)
+  file(GLOB_RECURSE files ${dir}/*)
+  foreach(file ${files})
+    copy_file_to_buildtree(${file} ${dir})
+  endforeach()
+
+  if(D3DCOMPILER_DLL)
+    get_filename_component(d3dcompiler_dir ${D3DCOMPILER_DLL} DIRECTORY)
+    copy_file_to_buildtree(${D3DCOMPILER_DLL} ${d3dcompiler_dir})
+  endif()
+
+  set(install_data ${install_data} PARENT_SCOPE)
+endfunction()
