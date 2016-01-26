6be3291f40569f5366621db45ddc6d2679d73033
fritsch <Peter.Fruehberger@gmail.com>
2016-01-26 07:20:13 +0100

    Revert "More radical solution"
    This reverts commit cf8e644ead431ea1023240224633447e3ee5da1e.

--- xbmc/cores/AudioEngine/Sinks/AESinkAUDIOTRACK.h
+++ xbmc/cores/AudioEngine/Sinks/AESinkAUDIOTRACK.h
@@ -68,6 +68,9 @@ private:
 
   // track raw sink delay to know if we over / underrun
   double                m_raw_sink_delay;
+  // while warming up make sure we don't cache more than the number of
+  // bytes we can write out - we can on paused buffer
+  unsigned int          m_raw_buffer_count_bytes;
 
   static CAEDeviceInfo m_info;
   static std::set<unsigned int>       m_sink_sampleRates;
