48235c85e2090e08cce0f4f1524c41039d580ae4
fritsch <peter.fruehberger@gmail.com>
2016-01-27 09:35:57 +0100

    Try fragmentation for AC3

--- xbmc/cores/AudioEngine/Sinks/AESinkAUDIOTRACK.cpp
+++ xbmc/cores/AudioEngine/Sinks/AESinkAUDIOTRACK.cpp
@@ -580,8 +580,12 @@ unsigned int CAESinkAUDIOTRACK::AddPackets(uint8_t **data, unsigned int frames, 
 	// error out as implementations cannot cope with with fragmented raw packages
         if (loop_written != size)
         {
+          if (m_format.m_streamInfo.m_type != CAEStreamInfo::STREAM_TYPE_AC3)
+          {
             CLog::Log(LOGERROR, "CAESinkAUDIOTRACK::AddPackets causes fragmentation of raw packages:  %d", loop_written);
             return INT_MAX;
+          }
+          CLog::Log(LOGDEBUG, "Trying AC3 fragmentation!");
         }
 
         m_duration_written += m_format.m_streamInfo.GetDuration() / 1000;
