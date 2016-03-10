ea4a5060a6c06fc37c4786ef2b3397e962c4e217
fritsch <fritsch@xbmc.org>
2016-03-10 14:41:34 +0100

    DelayRefresh (Android): Bring back the delay refreshrate

--- xbmc/windowing/egl/WinSystemEGL.cpp
+++ xbmc/windowing/egl/WinSystemEGL.cpp
@@ -56,6 +56,7 @@ CWinSystemEGL::CWinSystemEGL() : CWinSystemBase()
 
   m_egl               = NULL;
   m_iVSyncMode        = 0;
+  m_delayDispReset = false;
 }
 
 CWinSystemEGL::~CWinSystemEGL()
@@ -301,10 +302,13 @@ bool CWinSystemEGL::CreateNewWindow(const std::string& name, bool fullScreen, RE
   }
 
   {
-    CSingleLock lock(m_resourceSection);
-    // tell any shared resources
-    for (std::vector<IDispResource *>::iterator i = m_resources.begin(); i != m_resources.end(); ++i)
-      (*i)->OnResetDisplay();
+    if (!m_delayDispReset)
+    {
+      CSingleLock lock(m_resourceSection);
+      // tell any shared resources
+      for (std::vector<IDispResource *>::iterator i = m_resources.begin(); i != m_resources.end(); ++i)
+        (*i)->OnResetDisplay();
+    }
   }
 
   Show();
@@ -343,6 +347,12 @@ bool CWinSystemEGL::SetFullScreen(bool fullScreen, RESOLUTION_INFO& res, bool bl
 {
   CreateNewWindow("", fullScreen, res, NULL);
   CRenderSystemGLES::ResetRenderSystem(res.iWidth, res.iHeight, fullScreen, res.fRefreshRate);
+  int delay = CSettings::GetInstance().GetInt("videoscreen.delayrefreshchange");
+  if (delay > 0)
+  {
+    m_delayDispReset = true;
+    m_dispResetTimer.Set(delay * 100);
+  }
   int vsync_mode = CSettings::GetInstance().GetInt(CSettings::SETTING_VIDEOSCREEN_VSYNC);
   if (vsync_mode != VSYNC_DRIVER)
     SetVSyncImpl(m_iVSyncMode);
@@ -448,6 +458,14 @@ void CWinSystemEGL::PresentRenderImpl(bool rendered)
   if (!rendered)
     return;
   m_egl->SwapBuffers(m_display, m_surface);
+  if (m_delayDispReset && m_dispResetTimer.IsTimePast())
+  {
+    m_delayDispReset = false;
+    CSingleLock lock(m_resourceSection);
+    // tell any shared resources
+    for (std::vector<IDispResource *>::iterator i = m_resources.begin(); i != m_resources.end(); ++i)
+      (*i)->OnResetDisplay();
+  }
 }
 
 void CWinSystemEGL::SetVSyncImpl(bool enable)
