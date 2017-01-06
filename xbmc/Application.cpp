b7c7d6782d80c2ace9d36a9d5ece6527c7f3f28b
fritsch <Peter.Fruehberger@gmail.com>
2017-01-06 12:20:08 +0100

    Application: Throttle to 10 fps

--- xbmc/Application.cpp
+++ xbmc/Application.cpp
@@ -2761,12 +2761,12 @@ void CApplication::FrameMove(bool processEvents, bool processGUI)
     m_skipGuiRender = false;
     int fps = 0;
 
-#if defined(TARGET_RASPBERRY_PI) || defined(HAS_IMXVPU)
+//#if defined(TARGET_RASPBERRY_PI) || defined(HAS_IMXVPU)
     // This code reduces rendering fps of the GUI layer when playing videos in fullscreen mode
     // it makes only sense on architectures with multiple layers
     if (g_graphicsContext.IsFullScreenVideo() && !m_pPlayer->IsPausedPlayback() && m_pPlayer->IsRenderingVideoLayer())
-      fps = m_ServiceManager->GetSettings().GetInt(CSettings::SETTING_VIDEOPLAYER_LIMITGUIUPDATE);
-#endif
+      fps = 10;
+//#endif
 
     unsigned int now = XbmcThreads::SystemClockMillis();
     unsigned int frameTime = now - m_lastRenderTime;
