efa606a2d3c54d2da0b09f2066ab15f34774dde5
fritsch <Peter.Fruehberger@gmail.com>
2017-05-09 22:20:09 +0200

    AEPackIEC61937: Don't add the gap (optional)

--- xbmc/cores/AudioEngine/Utils/AEPackIEC61937.cpp
+++ xbmc/cores/AudioEngine/Utils/AEPackIEC61937.cpp
@@ -239,7 +239,7 @@ int CAEPackIEC61937::PackPause(uint8_t *dest, unsigned int millis, unsigned int 
     memcpy(dest+i*periodInBytes, dest, periodInBytes);
   }
 
-  packet->m_data[1] = (gap & 0x00FF) << 8;
-  packet->m_data[0] = (gap & 0xFF00) >> 8;
+//  packet->m_data[1] = (gap & 0x00FF) << 8;
+//  packet->m_data[0] = (gap & 0xFF00) >> 8;
   return periodsNeeded * periodInBytes;
 }
