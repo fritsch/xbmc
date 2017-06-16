caa0c87232db3ef6e45f42e3ccb12c1aed95759b
fritsch <Peter.Fruehberger@gmail.com>
2017-06-16 19:02:33 +0200

    Audiotrack: Disable float completely (shitty)

--- xbmc/cores/AudioEngine/Sinks/AESinkAUDIOTRACK.cpp
+++ xbmc/cores/AudioEngine/Sinks/AESinkAUDIOTRACK.cpp
@@ -311,7 +311,7 @@ bool CAESinkAUDIOTRACK::Initialize(AEAudioFormat &format, std::string &device)
   {
     m_passthrough = false;
     m_format.m_sampleRate     = m_sink_sampleRate;
-    if (CJNIAudioManager::GetSDKVersion() >= 21 && m_format.m_channelLayout.Count() == 2)
+    if (false && CJNIAudioManager::GetSDKVersion() >= 21 && m_format.m_channelLayout.Count() == 2)
     {
       m_encoding = CJNIAudioFormat::ENCODING_PCM_FLOAT;
       m_format.m_dataFormat     = AE_FMT_FLOAT;
@@ -828,7 +828,7 @@ void CAESinkAUDIOTRACK::EnumerateDevicesEx(AEDeviceInfoList &list, bool force)
       int test_sample[] = { 32000, 44100, 48000, 88200, 96000, 176400, 192000 };
       int test_sample_sz = sizeof(test_sample) / sizeof(int);
       int encoding = CJNIAudioFormat::ENCODING_PCM_16BIT;
-      if (CJNIAudioManager::GetSDKVersion() >= 21)
+      if (false && CJNIAudioManager::GetSDKVersion() >= 21)
         encoding = CJNIAudioFormat::ENCODING_PCM_FLOAT;
       for (int i=0; i<test_sample_sz; ++i)
       {
