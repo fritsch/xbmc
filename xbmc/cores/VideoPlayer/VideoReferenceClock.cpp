c8841f6c30ce4b351faeb67351d3e88b1b758ef9
peak3d <pfau@peak3d.de>
2020-01-16 15:43:04 +0100

    Prevent VideoSync from setting up in Destructor

--- xbmc/cores/VideoPlayer/VideoReferenceClock.cpp
+++ xbmc/cores/VideoPlayer/VideoReferenceClock.cpp
@@ -30,12 +30,14 @@ CVideoReferenceClock::CVideoReferenceClock() : CThread("RefClock")
   m_RefreshRate = 0.0;
   m_MissedVblanks = 0;
   m_VblankTime = 0;
+  m_vsyncStopEvent.Reset();
 
   Start();
 }
 
 CVideoReferenceClock::~CVideoReferenceClock()
 {
+  m_bStop = true;
   m_vsyncStopEvent.Set();
   StopThread();
 }
@@ -86,9 +88,13 @@ void CVideoReferenceClock::Process()
       m_VblankTime = Now;          //initialize the timestamp of the last vblank
       SingleLock.Leave();
 
-      m_vsyncStopEvent.Reset();
-      //run the clock
-      m_pVideoSync->Run(m_vsyncStopEvent);
+      // we might got signalled while we did not wait
+      if (!m_vsyncStopEvent.Signaled())
+      {
+        //run the clock
+        m_pVideoSync->Run(m_vsyncStopEvent);
+        m_vsyncStopEvent.Reset();
+      }
     }
     else
     {
